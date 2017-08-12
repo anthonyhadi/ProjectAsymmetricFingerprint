243f268d921a5a8a89ce38077654eaeefd24ba35
picolov <picolov@gmail.com>
2017-08-12 17:15:04 +0700

    add mobile and email field on register page

--- Application/src/main/java/com/example/android/asymmetricfingerprintdialog/RegisterActivity.java
+++ Application/src/main/java/com/example/android/asymmetricfingerprintdialog/RegisterActivity.java
@@ -18,6 +18,8 @@ public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
 
     @Bind(R.id.input_name) EditText _nameText;
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_mobile) EditText _mobileText;
     @Bind(R.id.input_password) EditText _passwordText;
     @Bind(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
     @Bind(R.id.btn_signup) Button _signupButton;
@@ -67,20 +69,14 @@ public class RegisterActivity extends AppCompatActivity {
         progressDialog.show();
 
         userId = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
         password = _passwordText.getText().toString();
 
-        // TODO: Implement your own signup logic here.
+        // TODO: call services /generatePin {userId,password,email,mobile}
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+
+        progressDialog.dismiss();
     }
 
     public void onSignupSuccess() {
@@ -105,6 +101,8 @@ public class RegisterActivity extends AppCompatActivity {
         boolean valid = true;
 
         String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
         String password = _passwordText.getText().toString();
         String reEnterPassword = _reEnterPasswordText.getText().toString();
 
@@ -113,6 +111,20 @@ public class RegisterActivity extends AppCompatActivity {
             valid = false;
         } else {
             _nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length()!=10) {
+            _mobileText.setError("Enter Valid Mobile Number");
+            valid = false;
+        } else {
+            _mobileText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
