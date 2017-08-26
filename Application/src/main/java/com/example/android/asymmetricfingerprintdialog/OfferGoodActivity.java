6efffc7f7242db16826bde1b7c1816112db75012
Anthony hadi <anthonyhadi90@gmail.com>
2017-08-26 14:26:06 +0700

    Update Anthony

---
+++ Application/src/main/java/com/example/android/asymmetricfingerprintdialog/OfferGoodActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.asymmetricfingerprintdialog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Emerio on 8/26/2017.
+ */
+
+public class OfferGoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
